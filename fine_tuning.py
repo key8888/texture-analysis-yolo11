7935012b8348196beba1ae6565de0c098ed6bf20
key8888 <aottware@gmail.com>
2025-03-23 22:51:01 +0900

    update fine_tuning.py

--- fine_tuning.py
+++ fine_tuning.py
@@ -2,28 +2,36 @@
 
 from ultralytics import YOLO
 from multiprocessing import freeze_support
+from pathlib import Path
 
+fine = Path("fine")
+runs = Path("runs")
 
-
-if __name__ == '__main__':
-    freeze_support()
+for spcdr in fine.iterdir():
+    # print(spcdr) # fine\no_spcdr_1
+    # print(spcdr.stem) # no_spcdr_1
     
-    # 1. モデルの読み込み（事前学習済み重みを利用）
-    model = YOLO("yolo11x-seg.pt")
+    if __name__ == '__main__':
+        freeze_support()
+        
+        # 1. モデルの読み込み（事前学習済み重みを利用）
+        model = YOLO("yolo11x-seg.pt")
 
-    # 2. トレーニングの実行
-    # ※ data.yaml のパスやエポック数、画像サイズなどはタスクに合わせて変更してください
-    results = model.train(
-        data="./fine/custom_dataset.yaml",  # data.yaml のファイルパス
-        epochs=600,                 # エポック数
-        imgsz=512,                  # 入力画像サイズ
-        batch=16,                   # バッチサイズ（必要に応じて変更）
-        lr0=0.005,                    # 初期学習率（例）
-        device="cuda",
-        project="./runs/segment",   # 保存場所
-        name="5d5_epochs_600"       # 保存フォルダ名
-    )
+        # 2. トレーニングの実行
+        # ※ data.yaml のパスやエポック数、画像サイズなどはタスクに合わせて変更してください
+        results = model.train(
+            data=spcdr/"custom_dataset.yaml",  # custom_dataset.yaml のファイルパス
+            epochs=1000,                 # エポック数
+            imgsz=512,                  # 入力画像サイズ
+            batch=16,                   # バッチサイズ（必要に応じて変更）
+            lr0=0.005,                    # 初期学習率（例）
+            device="cuda",
+            project=runs/"independ",   # 保存場所
+            name=spcdr.stem       # 保存フォルダ名
+        )
+        
+        print(f"{spcdr.stem}トレーニング終了")
 
-    # 3. トレーニング結果の表示
-    print("Training finished. Results:")
-    print(results)
+        # # 3. トレーニング結果の表示
+        # print("Training finished. Results:")
+        # print(results)
--- train.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolo11x-seg.pt")
-
-model.train(data="fine/custom_dataset.yaml", imgsz=512, device=0, batch=8, epochs=100)
